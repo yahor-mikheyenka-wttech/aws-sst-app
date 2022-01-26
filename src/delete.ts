f5d9d16905da09d16d32b9f754707a7dee34d273
yahor.mikheyenka <yahor.mikheyenka@cognifide.com>
2022-01-26 16:00:51 +0100

    auth API added

--- src/delete.ts
+++ src/delete.ts
@@ -6,8 +6,8 @@ export const main = handler(async (event) => {
     TableName: process.env.TABLE_NAME,
     // 'Key' defines the partition key and sort key of the item to be removed
     Key: {
-      userId: "123", // The id of the author
-      noteId: event.pathParameters.id, // The id of the note from the path
+			userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+			noteId: event.pathParameters.id, // The id of the note from the path
     },
   };
 
