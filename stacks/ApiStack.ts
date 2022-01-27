badd6a66575b3b5f65612a153864fc4d4fe7613b
yahor.mikheyenka <yahor.mikheyenka@cognifide.com>
2022-01-27 12:11:38 +0100

    Add a billing API

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -16,14 +16,18 @@ export default class ApiStack extends sst.Stack {
       defaultFunctionProps: {
         environment: {
           TABLE_NAME: table.tableName,
+					STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
         },
       },
       routes: {
+				// CRUD
 				"GET    /notes/{id}": "src/get.main",
-				"PUT    /notes/{id}": "src/update.main",
-				"DELETE /notes/{id}": "src/delete.main",
 				"GET    /notes": "src/list.main",
+				"PUT    /notes/{id}": "src/update.main",
         "POST   /notes": "src/create.main",
+				"DELETE /notes/{id}": "src/delete.main",
+				// stripe api
+				"POST   /billing": "src/billing.main",
       },
     });
 
