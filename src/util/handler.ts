af227a9aa2e8af523a5701a0ef5069726ced1ee4
yahor.mikheyenka <yahor.mikheyenka@cognifide.com>
2022-01-27 13:00:12 +0100

    update cors

--- src/util/handler.ts
+++ src/util/handler.ts
@@ -13,9 +13,13 @@ export default function handler(lambda) {
     }
 
     // Return HTTP response
-    return {
-      statusCode,
-      body: JSON.stringify(body),
-    };
+		return {
+			statusCode,
+			body: JSON.stringify(body),
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Credentials": true,
+			},
+		};
   };
 }
